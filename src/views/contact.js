659b00407d71f2e8d68133e82aef3496188f9c9f
Sashko Milushev <milushev.sashko@gmail.com>
2022-08-31 16:20:29 +0300

    Fix stilization of contact page

--- src/views/contact.js
+++ src/views/contact.js
@@ -3,23 +3,27 @@ import { html } from '../../lib.js'
 const contactTemplate = (onSubmit) => html`
     <section>
         <div class="contact">
-            <h2>Emai:</h2>
-            <h3>milushev.sashko@gmail.com</h3>
-            <h2>телефон:</h2>
-            <h3>+359885392121</h3>           
+            <h3>Email:    milushev.sashko@gmail.com</h3>
+            <h3>Телефон:    +359885392121</h3>           
         </div>
         <div>
             <form @submit=${onSubmit} class="submit-form">
-                <h2>Имате нужда от помощ с някоя рецепта? Не се колебайте да ме попитате за каквото и да било!</h2>
-                <label for="fname">Име</label>
-                <input type="text" id="fname" name="firstname" placeholder="Име..">
-                <label for="lname">Фамилия</label>
-                <input type="text" id="lname" name="lastname" placeholder="Фамилия..">
-                <label for="email">Email</label>
-                <input type="text" id="email" name="email" placeholder="Email..">
-                <label for="question">Запитване</label>
-                <textarea id="question" name="question" placeholder="Попитайте.." style="height:170px"></textarea>
-                <button type="submit">Прати</button>
+                <h3>Имате нужда от помощ с някоя рецепта? Не се колебайте да ме попитате за каквото и да било!</h3>
+                    <div class="column">
+                        <label for="fname">Име</label>
+                        <input type="text" id="fname" name="firstname" placeholder="Име..">
+                        <label for="lname">Фамилия</label>
+                        <input type="text" id="lname" name="lastname" placeholder="Фамилия..">
+                        <label for="email">Email</label>
+                        <input type="text" id="email" name="email" placeholder="Email..">
+                    </div>
+                    <div class="column">
+                    <label for="question">Запитване</label>
+                    <textarea id="question" name="question" placeholder="Попитайте.." style="height:170px"></textarea>
+                    <button type="submit">Прати</button>
+                    </div>
+                
+                
             </form>
         </div>
 

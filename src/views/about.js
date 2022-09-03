76264bbbbc3b691e26a0c3d352f1539ad18608f2
Sashko Milushev <milushev.sashko@gmail.com>
2022-09-03 12:17:10 +0300

    Add profile picture in the about page. Change CSS for the green color in nav and buttons.

--- src/views/about.js
+++ src/views/about.js
@@ -2,9 +2,7 @@ import { html } from '../../lib.js'
 
 const aboutTemplate = () => html`
     <section id="about">
-        <h1>
-            Here is information about me
-        </h1>
+        <img src="../src/images/profile.jpg" alt="profile picture" class="about-pic">
 
     </section>
 `

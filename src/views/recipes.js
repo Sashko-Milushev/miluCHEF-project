da3503819f2b22492fd7528d00e7a1426a38155c
Sashko Milushev <milushev.sashko@gmail.com>
2022-09-05 17:48:50 +0300

    Refactor recipes page

--- src/views/recipes.js
+++ src/views/recipes.js
@@ -1,28 +1,30 @@
 import { html } from '../../lib.js'
 
 const recipesTemplate = () => html`
-    <div class="wrapper">
-        <div class="box1">
-            <img src="../src/images/recipes.jpg" alt="dish pic">
-            <button class="btn">Салати и разядки</button>
+    <div class="grid">
+        <div class="grid-item">
+            <div class="container">
+                <img src="../src/images/recipes.jpg" alt="Snow">
+                <button class="btn">Button</button>
+            </div>
         </div>
-        <div class="box2">
+        <div class="grid-item">
             <img src="../src/images/recipes.jpg" alt="dish pic">
             <button class="btn">Постни ястия</button>
         </div>
-        <div class="box3">
+        <div class="grid-item">
             <img src="../src/images/recipes.jpg" alt="dish pic">
             <button class="btn">Ястия с месо и риба</button>
         </div>
-        <div class="box4">
+        <div class="grid-item">
             <img src="../src/images/recipes.jpg" alt="dish pic">
             <button class="btn">Печива и десерти</button>
         </div>
-        <div class="box5">
+        <div class="grid-item">
             <img src="../src/images/recipes.jpg" alt="dish pic">
             <button class="btn">Зимнина и консерви</button>
         </div>
-        <div class="box6">
+        <div class="grid-item">
             <img src="../src/images/recipes.jpg" alt="dish pic">
             <button class="btn">Напитки</button>
         </div>
